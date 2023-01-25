d5be551feec99dfeedcb3826b96dcf49a1890bf1
Bailey Sostek <bhsostek@wpi.edu>
2023-01-25 10:10:38 -0500

    Initial Commit of Project

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+// Imports go at the top of the file, you are importing code form other modules.
+import * as fs from 'fs'; // Import File system so we can read and write files
+import { Grammars } from  'ebnf' // Import EBNF so that we can make our grammar. 
+
+// Load our grammar file from the disk into the variable "file"
+let file = fs.readFileSync('./res/grammar.ebnf');
+
+// Pass that file as a string to the EBNF parser creator.
+let parser = new Grammars.BNF.Parser(file.toString());
+
+// Use our parser to parse a string.
+console.log(parser.getAST( '(2 + (2 * -123)) * 5332'));
