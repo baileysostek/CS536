0a376a1e143ecc2b31d9c64df481dd074d971be5
ThePiratePup <jdboucher@wpi.edu>
2023-02-01 17:12:27 -0500

    Added ebnf files

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,12 @@ import * as fs from 'fs'; // Import File system so we can read and write files
 import { Grammars } from  'ebnf' // Import EBNF so that we can make our grammar. 
 
 // Load our grammar file from the disk into the variable "file"
-let file = fs.readFileSync('./res/grammar.ebnf');
+let file = fs.readFileSync('./res/simplifiedJavascript.bnf');
 
 // Pass that file as a string to the EBNF parser creator.
 let parser = new Grammars.BNF.Parser(file.toString());
 
 // Use our parser to parse a string.
-console.log(parser.getAST( '(2 + (2 * -123)) * 5332'));
+//console.log(parser.getAST( '(2 + (2 * -123)) * 5332'));
 
 var test : String = ""
