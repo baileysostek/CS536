cac85148e60bfd2a3fc410ad454a019e0d4b263a
Bailey Sostek <bhsostek@wpi.edu>
2023-02-19 16:57:28 -0500

    Recursive functions now work. Added some Math Functions

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,40 @@ function App() {
     console.log(messsage.value);
   })
 
+  registerFunction("print", (...parameters) => {
+    console.log(...parameters)
+  })
+
+  registerFunction("add", (a : number, b : number) => {
+    let value = a + b;
+    return value;
+  })
+
+  registerFunction("sub", (a : number, b : number) => {
+    let value = a - b;
+    return value;
+  })
+
+  registerFunction("mul", (a : number, b : number) => {
+    let value = a * b;
+    return value;
+  })
+
+  registerFunction("div", (a : number, b : number) => {
+    let value = a / b;
+    return value;
+  })
+
+  registerFunction("mod", (a : number, b : number) => {
+    let value = a % b;
+    return value;
+  })
+
+  registerFunction("pow", (a : number, b : number) => {
+    let value = Math.pow(a, b);
+    return value;
+  })
+
   return (
     <div className="App">
       <header className="App-header">
